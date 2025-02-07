f6f14ff88b2aa7c12b919fa0030f5d9bca0866c1
Theunique-1 <udopeter882@gmail.com>
2025-02-07 14:20:47 -0800

    updated file again

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -17,9 +17,7 @@ email VARCHAR(215) UNIQUE,
 address TEXT);
 
 CREATE TABLE Orders(order_id INT AUTO_INCREMENT PRIMARY KEY,
-customer_id INT,
-
-FOREIGN KEY(customer_id)REFERENCES Customers(customer_id),
+customer_id INT,FOREIGN KEY(customer_id)REFERENCES Customers(customer_id),
 
 order_date DATE);
 
